39b88e77fa9c970eb81b702bc88e409eb32be9be
Yi-Wei Chang <jchang6513@gmail.com>
2021-07-24 14:15:08 +0800

    Extract line noty function

--- dir-size-alarm.sh
+++ dir-size-alarm.sh
@@ -2,26 +2,30 @@
 source .dsa.conf
 # df $1 | awk '{ print $4 }' | tail -n 1 | numfmt --from=iec-i
 
+lineNoty() {
+	curl --location --request POST 'https://notify-api.line.me/api/notify' \
+		--header "Authorization: Bearer $LINE_TOKEN" \
+		--form "message=\"Warning!!! The available size of $1 is under ${2}\""
+
+	curl --location --request POST 'https://notify-api.line.me/api/notify' \
+		--header "Authorization: Bearer $LINE_TOKEN" \
+		--form "message=\"$(df -h $1)\""
+
+	}
+
 while read line; do
-    dirPath=$(echo $line| awk -F ' ' '{print $1}')
-    threshold=$(echo $line | awk -F ' ' '{print $2}')
+	dirPath=$(echo $line| awk -F ' ' '{print $1}')
+	threshold=$(echo $line | awk -F ' ' '{print $2}')
 
-    avail=$(df -h $dirPath | awk '{ print $4 }' | tail -n 1)
+	avail=$(df -h $dirPath | awk '{ print $4 }' | tail -n 1)
 
-    echo $dirPath, $avail, $threshold, $(numfmt --from=$NUMFMT_FROM $threshold)
-    
-    if (( $(numfmt --from=$NUMFMT_FROM $avail)  > $(numfmt --from=$NUMFMT_FROM $threshold) )); then
-	echo OK; 
-    else 
-	curl --location --request POST 'https://notify-api.line.me/api/notify' \
-	     --header "Authorization: Bearer $LINE_TOKEN" \
-	     --form "message=\"Warning!!! The available size of $dirPath is under ${threshold}\""
+	echo $dirPath, $avail, $threshold, $(numfmt --from=$NUMFMT_FROM $threshold)
 
-	curl --location --request POST 'https://notify-api.line.me/api/notify' \
-	     --header "Authorization: Bearer $LINE_TOKEN" \
-	     --form "message=\"$(df -h $dirPath)\""
-
-	echo NOT OK !!!; 
-    fi
+	if (( $(numfmt --from=$NUMFMT_FROM $avail)  > $(numfmt --from=$NUMFMT_FROM $threshold) )); then
+		echo OK; 
+	else 
+		lineNoty $dirPath $threshold
+		echo NOT OK !!!; 
+	fi
 done < dirs.txt
 
